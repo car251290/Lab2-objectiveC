a1c8bb32af11b2e83f01bea895e0b365459d3610
Javier Carreño <carrenojavier015@gmail.com>
2018-09-05 09:17:54 -0700

    Initial Commit

---
+++ lab2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  lab2
+//
+//  Created by Javier Carreno Reyna on 2018-09-05.
+//  Copyright © 2018 Javier Carreno Reyna. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
